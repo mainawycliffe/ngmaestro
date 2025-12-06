349b348770b04a09a026617d78af361fdc9a89ee
Maina Wycliffe <wmmaina7@gmail.com>
2025-12-06 23:15:57 +0300

    feat: add timeout configuration to theOracle function for improved performance

--- apps/functions/src/index.ts
+++ apps/functions/src/index.ts
@@ -2,4 +2,9 @@ import { onCallGenkit } from 'firebase-functions/https';
 import './config';
 import { theOracleFlow } from './flows';
 
-export const theOracle = onCallGenkit(theOracleFlow);
+export const theOracle = onCallGenkit(
+  {
+    timeoutSeconds: 5 * 60,
+  },
+  theOracleFlow,
+);
